f6e716e083649041f88930700f6f975489aaa4c5
MaddnRuleZ <106453966+MaddnRuleZ@users.noreply.github.com>
2024-03-31 18:26:15 +0200

    qawe

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ app = Flask(__name__)
 @app.route('/cpu-usage')
 def cpu_usage():
     cpu_percent = psutil.cpu_percent(interval=1)
+    print("Returning")
     return json.dumps({'cpu_percent': cpu_percent})
 
 if __name__ == '__main__':
